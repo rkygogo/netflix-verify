c12f5c563b70f8915de14f64a605fbd15ff68299
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-09-21 09:54:05 +0800

    Update ipv6.go

--- verify/ipv6.go
+++ verify/ipv6.go
@@ -16,9 +16,14 @@ type IPv6Verifier struct {
 func (v *IPv6Verifier) Execute() *VerifyResponse {
 	var err error
 	var response VerifyResponse
-	v.unblockStatus = AreaUnavailable
-	response.Type = 2
-
+        if res.CountryCode != "" {
+			v.unblockStatus = AreaUnavailable
+	                response.Type = 2
+			response.CountryCode = res.CountryCode
+			response.CountryName = util.CountryCodeToCountryName(res.CountryCode)
+		} 
+	
+	
 	if v.IP, err = util.DnsResolver(6); err != nil {
 		response.StatusCode = NetworkUnrachable
 		return &response
