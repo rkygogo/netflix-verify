8c7d9c1da00875fdb6c72a5a606645683486e4dd
rkygogo <107276912+rkygogo@users.noreply.github.com>
2022-09-21 10:37:28 +0800

    Update ipv4.go

--- verify/ipv4.go
+++ verify/ipv4.go
@@ -27,35 +27,6 @@ func (v *IPv4Verifier) Execute() *VerifyResponse {
 	v.unblockTestChan = make(chan UnblockTestResult)
 	defer close(v.unblockTestChan)
 
-	if v.Custom == "" {
-		go v.UnblockTest(AreaAvailableID)
-		go v.UnblockTest(SelfMadeAvailableID)
-		go v.UnblockTest(NonSelfMadeAvailableID)
-
-		for i := 0; i < 3; i++ {
-			switch res := <-v.unblockTestChan; {
-
-			case res.err != nil:
-				v.unblockStatus = NetworkUnrachable
-
-			case res.CountryCode != "":
-				switch res.movieID {
-				case AreaAvailableID:
-					v.upgradeStatus(AreaAvailable)
-				case SelfMadeAvailableID:
-					v.upgradeStatus(UnblockSelfMadeMovie)
-				case NonSelfMadeAvailableID:
-					v.upgradeStatus(UnblockNonSelfMadeMovie)
-				}
-				response.CountryCode = res.CountryCode
-				response.CountryName = util.CountryCodeToCountryName(res.CountryCode)
-			default:
-			}
-		}
-	} else {
-		if customMovieID, err := strconv.Atoi(v.Custom); err == nil {
-			go v.UnblockTest(customMovieID)
-		}
 		res := <-v.unblockTestChan
 		if res.CountryCode != "" {
 			v.unblockStatus = CustomMovieUnblock
